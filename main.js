ad54df69d538f752d1d7af75952b40b347661641
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-03-16 14:44:51 +0100

    1.0

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import './style.css';
 import * as THREE from 'three';
-import imga from '/sus.png';
-import susSFX from '/amogus.ogg';
+import imga from './sus.png';
+import susSFX from './amogus.ogg';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 let sussus;
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('c'),
+    canvas: document.querySelector('#c'),
     antialias: true
 });
 camera.position.z = 30;
@@ -15,17 +15,23 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 window.addEventListener( 'resize', onWindowResize );
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
 const texture = new THREE.TextureLoader().load(imga);
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial({
     map: texture
 });
-
+let susSpread = 30;
 const ambientlight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientlight);
 
 if (localStorage.getItem('sussus')) {
     sussus = Number(localStorage.getItem('sussus'));
+    for (var i; i<sussus; i++) {
+        camera.z+= 0.1;
+        susSpread+= 0.1;
+
+    }
 } else {
     sussus = 0;
 }
@@ -37,11 +43,12 @@ function createAmogus() {
     amogus.rotation.x = xr;
     amogus.rotation.y = yr;
     amogus.rotation.z = zr;
-    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(30));
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(susSpread));
     amogus.position.x = x;
     amogus.position.y = y;
     amogus.position.z = z;
     scene.add(amogus);
+    document.querySelector('#counter').innerHTML = sussus;
 }
 
 function onWindowResize() {
